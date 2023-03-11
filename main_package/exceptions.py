dd90a25e895ca9f4a23d0e6ae1a7062480f7fa45
marie-george <maria85night@gmail.com>
2023-03-12 01:41:32 +0300

    added class exceptions

---
+++ main_package/exceptions.py
@@ -0,0 +1,13 @@
+class InstantiateCSVError(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = 'Файл item.csv поврежден'
+
+    def __str__(self):
+        return self.message
+
+# e = InstantiateCSVError
+# print(e)
